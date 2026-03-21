d97f00477cb3a0501665e92aecb214ceae0e77ec
lotva <dlotva@gmail.com>
2026-03-21 14:10:10 +0300

    refactor: add `src` path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'node:url'
 import vike from 'vike/plugin'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
 	plugins: [vike(), react()],
+
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('src', import.meta.url)),
+		},
+	},
 })
